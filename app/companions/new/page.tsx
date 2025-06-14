25c63115f726c68c0aeed711488e4806301681cd
vinsmokejazz <vinsmokejazz@gmail.com>
2025-06-15 00:07:35 +0530

    integrated supabase

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,13 +1,20 @@
 'use client'
 import CompanionForm from '@/components/CompanionForm'
-import React from 'react'
+import { auth } from '@clerk/nextjs/server';
+import { redirect } from "next/navigation";
+import React, { use } from 'react'
 import { z } from "zod";
 
 const formSchema = z.object({
   username: z.string().min(2).max(50),
 })
 
-const NewCompanion = () => {
+const NewCompanion = async() => {
+  const {userId} = await auth();
+  if(!userId) redirect('/sign-in');
+
+
+
   return (
     <main className='min-lg:w-1/3 minmd:w-2/3 items-center justify-center'>
       <article className='w-full gap-4 flex flex-col'>
