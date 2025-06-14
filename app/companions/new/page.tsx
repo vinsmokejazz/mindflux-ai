16e333bb9e3ddcf1bc411c8aa6300b616ce1aefa
vinsmokejazz <vinsmokejazz@gmail.com>
2025-06-14 14:35:19 +0530

    completed Form with shadcn library

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,10 +1,20 @@
+'use client'
+import CompanionForm from '@/components/CompanionForm'
 import React from 'react'
+import { z } from "zod";
+
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+})
 
 const NewCompanion = () => {
   return (
-    <div>
-      new Companion
-    </div>
+    <main className='min-lg:w-1/3 minmd:w-2/3 items-center justify-center'>
+      <article className='w-full gap-4 flex flex-col'>
+        <h1>Companion Builder</h1>
+        <CompanionForm />
+      </article>
+    </main> 
   )
 }
 
